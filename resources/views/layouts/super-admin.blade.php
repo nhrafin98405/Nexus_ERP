6d3c2352307633113e4af8784b782b8e302eebf7
NH Rafin <nhrafin98405@gmail.com>
2026-07-08 03:26:34 +0600

    ERP Template Integration Done

--- resources/views/layouts/super-admin.blade.php
+++ resources/views/layouts/super-admin.blade.php
@@ -8,51 +8,53 @@
 {{-- head --}}
 
 <body class="bg-theme bg-theme1">
-	<!--wrapper-->
-	<div class="wrapper">
-		<!--sidebar wrapper -->
+    <!--wrapper-->
+    <div class="wrapper">
+        <!--sidebar wrapper -->
 
         @include('includes.sidebar')
 
-		<!--end sidebar wrapper -->
-		<!--start header -->
+        <!--end sidebar wrapper -->
+        <!--start header -->
 
 
         @include('includes.header')
 
-		<!--end header -->
-		<!--start page wrapper -->
+        <!--end header -->
+        <!--start page wrapper -->
+
+        <div class="page-wrapper">
+            <div class="page-content">
+
+                @yield('content')
 
 
+            </div>
+        </div>
 
-@yield('content')
-
-
-
-
-		<!--end page wrapper -->
-		<!--start overlay-->
-		<div class="overlay toggle-icon"></div>
-		<!--end overlay-->
-		<!--Start Back To Top Button--> <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
-		<!--End Back To Top Button-->
+        <!--end page wrapper -->
+        <!--start overlay-->
+        <div class="overlay toggle-icon"></div>
+        <!--end overlay-->
+        <!--Start Back To Top Button--> <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
+        <!--End Back To Top Button-->
 
 
         @include('includes.footer')
 
-	</div>
-	<!--end wrapper-->
-	<!--start switcher-->
-	
+    </div>
+    <!--end wrapper-->
+    <!--start switcher-->
+
     @include('includes.switch')
 
 
-	<!--end switcher-->
-	<!-- Bootstrap JS -->
-	
-@include('includes.scripts')
+    <!--end switcher-->
+    <!-- Bootstrap JS -->
 
-<!-- Bootstrap JS -->
+    @include('includes.scripts')
+
+    <!-- Bootstrap JS -->
 </body>
 
 </html>
