ec0473b21bf68b1242c9b5e2fac89118650f02e6
NH Rafin <nhrafin98405@gmail.com>
2026-07-11 04:51:33 +0600

    Add reusable delete modal component and update role menu management

--- resources/views/super-admin/settings/components/delete-modal.blade.php
+++ resources/views/super-admin/settings/components/delete-modal.blade.php
@@ -1,60 +1,43 @@
-<div class="modal fade"
-     id="{{ $id }}"
-     tabindex="-1"
-     aria-hidden="true">
-
-    <div class="modal-dialog">
-
-        <div class="modal-content">
+<div class="modal fade" id="{{ $id }}" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content bg-danger">
 
             <div class="modal-header">
-
-                <h5 class="modal-title">
+                <h5 class="modal-title text-white">
                     Confirm Delete
                 </h5>
 
                 <button type="button"
                         class="btn-close"
-                        data-bs-dismiss="modal">
+                        data-bs-dismiss="modal"
+                        aria-label="Close">
                 </button>
-
             </div>
 
-            <div class="modal-body">
-
+            <div class="modal-body text-white">
                 {{ $message ?? 'Are you sure you want to delete this record?' }}
-
             </div>
 
             <div class="modal-footer">
 
                 <button type="button"
-                        class="btn btn-secondary"
+                        class="btn btn-light"
                         data-bs-dismiss="modal">
-
                     Cancel
-
                 </button>
 
-                <form action="{{ $action }}"
-                      method="POST">
-
+                <form action="{{ $action }}" method="POST">
                     @csrf
                     @method('DELETE')
 
-                    <button type="button"
-        class="btn btn-sm btn-light"
-        data-bs-toggle="modal"
-        data-bs-target="#deleteRole{{ $role->id }}">
-    Delete
-</button>
-
+                    <button type="submit" class="btn btn-light">
+                        <i class="bx bx-trash"></i>
+                        Delete
+                    </button>
                 </form>
 
             </div>
 
         </div>
-
     </div>
-
 </div>
